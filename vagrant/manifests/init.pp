ee80db8eb70b5859f94931898a30cd10c5fe09c6
Dean Reilly <me@deanreilly.co.uk>
2013-09-09 10:58:05 +0100

    Added some notes to the puppet manifest.

--- vagrant/manifests/init.pp
+++ vagrant/manifests/init.pp
@@ -1,8 +1,14 @@
 package { "xfce4" : }
 package { "gdm" : }
 #package { "wine" : }
-# sudo add-apt-repository ppa:ubuntu-wine/ppa -y && sudo apt-get update && sudo apt-get install wine
+# run:
+#   dpkg --add-architecture i386
+#   enable multiverse
+#   apt-get update
+#   apt-get install wine1.4 wine-gecko1.4
 
 # Install python tesseract.
 package{ "tesseract-ocr" : }
+
+# install cnee, mousepad
 
