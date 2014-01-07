4fcf03a0887b20b2dcd8d3b7b8513983140673b4
Dean Reilly <me@deanreilly.co.uk>
2014-01-07 23:50:07 +0000

    Replace base32 code with the python library.

--- mobilepass.py
+++ mobilepass.py
@@ -1,11 +1,7 @@
-import hmac
-import base64
 import array
-import binascii
-import re
-from array import array
-
+import base64
 import hashlib
+import hmac
 
 # I ported the KDF1 algorithm from the bouncycastle library that shipped with
 # the app as the python libraries that included this function seemed to be
@@ -51,54 +47,14 @@ def KDF1(hash, secret, iv, start_position, key_length): #key should be passed by
 		counter += 1
 		digest_counter += 1
 
-def validate_and_normalize(activation_code):
-
-	if len(activation_code) == 0:
-		return activation_code
-
-	activation_code = activation_code.strip().upper()
-
-	counter = 1
-	checksum = 0
-	normalized_code = ""
-
-	for character in activation_code:
-
-		if character == '0':
-			character = 'O'
-
-		if character == '1':
-			character = 'I'
-
-		if re.match("[A-Z2-7=]", character) is None:
-			continue;
-
-		if counter % 5 == 0:
-			# Process checksum digit.
-			if base32_lookup(character) != (checksum % 32):
-				return "Error"
-
-			checksum = 0;
-
-		else:
-			normalized_code += character
-			checksum += ord(character) * (counter % 5);
-
-		counter += 1
-
-	return normalized_code
-
-def base32_lookup(character):
-	base32_encoding = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8,
-	                   'J':9, 'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16,
-	                   'R':17, 'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24,
-	                   'Z':25, '2':26, '3':27, '4':28, '5':29, '6':30, '7':31}
-
-	return base32_encoding[character]
-
 def get_entropy(activation_code):
-	normalized_code = validate_and_normalize(activation_code)
-	return base64.b32decode(normalized_code)
+	# Remove hyphens.
+	activation_code = activation_code.replace('-', '')
+	
+	# Remove every 5th chracter.
+	activation_code = ''.join('' if i % 5 == 0 else char for i, char in enumerate(activation_code, 1))
+
+	return base64.b32decode(activation_code, True, "I")
 
 def get_key(entropy, policy):
 	secret = entropy
@@ -115,7 +71,7 @@ def long_to_byte_array(long_num):
     """
     helper function to convert a long number into a byte array
     """
-    byte_array = array('B')
+    byte_array = array.array('B')
     for i in reversed(range(0, 8)):
         byte_array.insert(0, long_num & 0xff)
         long_num >>= 8
@@ -130,10 +86,10 @@ def truncated_value(h):
 
 key = "QVKYC-FM6KO-SY6F7-TR22W"
 policy = ""
+message = long_to_byte_array(0)
 
 entropy = get_entropy(key)
 key = get_key(entropy, policy)
-message = long_to_byte_array(0)
 
 h = hmac.new(key, message, hashlib.sha256).hexdigest()
 h = truncated_value(h)
