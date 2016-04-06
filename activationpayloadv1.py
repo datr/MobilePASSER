683843fa9213d13bd89591d7b1d9e802e016b763
Dean Reilly <me@deanreilly.co.uk>
2016-04-07 00:01:03 +0100

    Don't check code length for legacy activation codes.

--- activationpayloadv1.py
+++ activationpayloadv1.py
@@ -4,9 +4,6 @@ class ActivationPayloadV1:
 	
 	def __init__(self, activation_code, compatability_level=False):
 		if compatability_level != False:
-			if len(activation_code) != self.getByteLength():
-				raise ValueError("activation code is the wrong length")
-
 			self.payloadID = 1
 			self.entropy = activation_code
 			self.capabilityLevel = compatability_level
